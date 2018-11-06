80278f356a9ab5736e73391ab2dc47c1b2e002f0
shind07 <shind@sfu.ca>
2018-11-06 11:21:13 -0800

    Remove sessions stuff

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-  alert("working");
+  var host;
+  var login_url;
 
+  // Load config, update dynamic figures
   $.getJSON("config.json", function(data) {
-    var host = data.host
-    console.log(host);
+    host = data.host
+
 
     $(".dynamic").each(function() {
         var url = host + "/" +  $(this).data('task')
-        console.log(url);
         var self = $(this);
         $.getJSON(url, function(data) {
-          console.log(self.text() + "asgd")
           self.text(JSON.stringify(data));
           console.log(JSON.stringify(data));
         })
-
     })
-
   });
+
 
 });
