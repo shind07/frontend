e7047045f3e62294ae5c0fc216dfd057f1b4dc3b
shind07 <shind@sfu.ca>
2018-11-07 08:41:51 -0800

    Add support for google charts

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  var host;
-  var login_url;
+  google.charts.load('current', {packages: ['corechart']});
+  google.charts.setOnLoadCallback(loadData);
+});
 
-  // Load config, update dynamic figures
-  $.getJSON("config.json", function(data) {
-    host = data.host
+function loadData() {
+  $.getJSON('config.json', function(data) {
+    var host = data.host
 
     $("div.dynamic").each(function() {
       var self = $(this);
@@ -12,12 +13,34 @@ $(document).ready(function() {
 
       $.getJSON(url, function(data) {
         var str_data = JSON.stringify(data);
-        var obj = JSON.parse(str_data);
+        var json_data = JSON.parse(str_data);
+        console.log(data);
+        console.log(str_data);
+        console.log(json_data);
 
         if (self.data('type') == "table") {
           self.children("table").html(arrayToTable(data));
         }
+        else if (self.data('type') == "chart") {
+          var chart_data = google.visualization.arrayToDataTable(json_data, false);
+          var chart = new google.visualization.LineChart(document.getElementById(self.attr('id')));
+          var options = {
+            backgroundColor: {
+            //   fill:"#a2a3aa",
+              stroke:"#a2a3aa",
+            //   strokeWidth:2
+            },
+            chartArea: {
+              backgroundColor:"#a2a3aa"
+            },
+            hAxis: {
+              direction: -1
+            },
+
+          };
+          chart.draw(chart_data, options);
+        }
       });
     });
   });
-});
+}
