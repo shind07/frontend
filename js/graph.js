3a485f00115047e8110735a12dba400baded240c
shind07 <shind@sfu.ca>
2018-11-12 12:24:16 -0800

    Add default sizes for charts

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,6 @@
+var defaultWidth = 800;
+var defaultHeight = 400;
+
 function createLineGraph(chartID, data) {
   var chart_data = google.visualization.arrayToDataTable(data, false);
   var chart = new google.visualization.LineChart(document.getElementById(chartID));
@@ -13,7 +16,8 @@ function createLineGraph(chartID, data) {
     hAxis: {
       direction: -1
     },
-    width: '100%',
+    width: defaultWidth,
+    height: defaultHeight,
 
   };
   chart.draw(chart_data, options);
@@ -22,7 +26,11 @@ function createLineGraph(chartID, data) {
 function createScatterPlot(chartID, data) {
   var chart_data = google.visualization.arrayToDataTable(data, false);
   var chart = new google.visualization.ScatterChart(document.getElementById(chartID));
-  chart.draw(chart_data);
+  var options = {
+    width: defaultWidth,
+    height: defaultHeight,
+  }
+  chart.draw(chart_data, options);
 }
 
 function createHistgram(chartID, data) {
@@ -30,7 +38,8 @@ function createHistgram(chartID, data) {
     var chart = new google.visualization.Histogram(document.getElementById(chartID));
     var options = {
        histogram: { bucketSize: .05 },
-       width: '100%',
+       width: defaultWidth,
+       height: defaultHeight,
      };
     chart.draw(chart_data, options);
 }
