46a42f6849e8fd96682adca52770612bfd6d7c83
shind07 <shind@sfu.ca>
2018-11-13 11:03:03 -0800

    Tweak chart styling

--- js/graph.js
+++ js/graph.js
@@ -5,14 +5,6 @@ function createLineGraph(chartID, data) {
   var chart_data = google.visualization.arrayToDataTable(data, false);
   var chart = new google.visualization.LineChart(document.getElementById(chartID));
   var options = {
-    backgroundColor: {
-    //   fill:"#a2a3aa",
-      stroke:"#a2a3aa",
-    //   strokeWidth:2
-    },
-    chartArea: {
-      backgroundColor:"#a2a3aa"
-    },
     hAxis: {
       direction: -1
     },
